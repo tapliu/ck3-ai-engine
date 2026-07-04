f29c948ccfb6a35d64faa2946eb4bc888370f6e6
tapliu <tapliu@163.com>
2026-07-04 14:58:39 +0800

    feat: separate base & bonus stats display (武力 102=97+5)

--- app/models/character.py
+++ app/models/character.py
@@ -7,10 +7,14 @@ class Character:
         self.id = cid
         self.name = name
         self.gender = gender
-        self.l = l
-        self.w = w
-        self.i = i
-        self.p = p
+        self.base_l = l
+        self.base_w = w
+        self.base_i = i
+        self.base_p = p
+        self.bonus_l = 0
+        self.bonus_w = 0
+        self.bonus_i = 0
+        self.bonus_p = 0
         self.age = age
 
         self.alive = True
@@ -24,6 +28,22 @@ class Character:
         self.treasures = []
 
         self.memory = []
+
+    @property
+    def l(self):
+        return self.base_l + self.bonus_l
+
+    @property
+    def w(self):
+        return self.base_w + self.bonus_w
+
+    @property
+    def i(self):
+        return self.base_i + self.bonus_i
+
+    @property
+    def p(self):
+        return self.base_p + self.bonus_p
 
     def score(self):
         return self.l*0.25 + self.w*0.35 + self.i*0.2 + self.p*0.2
@@ -41,6 +61,14 @@ class Character:
             "w": self.w,
             "i": self.i,
             "p": self.p,
+            "base_l": self.base_l,
+            "base_w": self.base_w,
+            "base_i": self.base_i,
+            "base_p": self.base_p,
+            "bonus_l": self.bonus_l,
+            "bonus_w": self.bonus_w,
+            "bonus_i": self.bonus_i,
+            "bonus_p": self.bonus_p,
             "score": round(self.score(), 2),
             "alive": self.alive,
             "spouse": self.spouse,
